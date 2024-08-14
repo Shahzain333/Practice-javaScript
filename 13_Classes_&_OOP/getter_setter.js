f941fa8e81bca4907a1da55a0785dbc8ecc027fc
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-08-14 12:06:17 +0500

    Update getter_setter.js

--- 13_Classes_&_OOP/getter_setter.js
+++ 13_Classes_&_OOP/getter_setter.js
@@ -1,3 +1,35 @@
+// old syntax
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
+
+
+// New version 
+
 class Person {
     #name;
     #age;
@@ -32,32 +64,3 @@ class Person {
   console.log(person.name);       // 'Alice'
 //   console.log(person.#name); // SyntaxError: Private field '#name' must be declared in an enclosing class
   
-
-// old syntax
-
-function User(email, password){
-    this._email = email;
-    this._password = password
-
-    Object.defineProperty(this, 'email', {
-        get: function(){
-            return this._email.toUpperCase()
-        },
-        set: function(value){
-            this._email = value
-        }
-    })
-    Object.defineProperty(this, 'password', {
-        get: function(){
-            return this._password.toUpperCase()
-        },
-        set: function(value){
-            this._password = value
-        }
-    })
-
-}
-
-const chai = new User("chai@chai.com", "chai")
-
-console.log(chai.email);
